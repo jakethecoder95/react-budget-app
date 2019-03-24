eff056f297498e53c0af5b8c07b664eddf8c586b
jakethecoder95 <95jacob07@gmail.com>
2019-03-23 22:20:49 -0700

    BudgetOverview style complete on desktop

--- client/src/components/Bottom/BudgetOverview.js
+++ client/src/components/Bottom/BudgetOverview.js
@@ -3,24 +3,31 @@ import React from "react";
 
 const BudgetOverview = props => {
   return (
-    <div className="ui container overview">
+    <div className="ui container">
       <div className="overview">
         <div className="income">
-          <h3 className="inc-header">Income</h3>
-          <div className="item item-inc">
-            <div>Income</div>
-            <div>$1,500.00</div>
+          <div className="overview__section-header">
+            <i className="dropdown icon" />
+            <h3 className="inc-header">Income</h3>
+          </div>
+          <div items>
+            <div className="item item-inc">
+              <div>Income</div>
+              <div>$1,500.00</div>
+            </div>
           </div>
         </div>
         <div className="expences">
           <h3 className="exp-header">Expences</h3>
-          <div className="item item-exp">
-            <div>Gas</div>
-            <div>$45.00</div>
-          </div>
-          <div className="item item-exp">
-            <div>Gas</div>
-            <div>$45.00</div>
+          <div classname="items">
+            <div className="item item-exp">
+              <div>Gas</div>
+              <div>$45.00</div>
+            </div>
+            <div className="item item-exp">
+              <div>Gas</div>
+              <div>$45.00</div>
+            </div>
           </div>
         </div>
       </div>
