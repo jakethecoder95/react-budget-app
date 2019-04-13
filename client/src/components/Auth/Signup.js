af4b326049acd561cf099249b2ff75ba96f4470c
jakethecoder95 <95jacob07@gmail.com>
2019-04-13 13:50:07 -0700

    to be rebased

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -2,10 +2,32 @@ import "./Form.css";
 import React, { Fragment } from "react";
 import { Form, Field } from "react-final-form";
 import { connect } from "react-redux";
+import hasError from "../Bottom/Form/util/has-error";
 
 const Signup = props => {
   const onSubmit = userInfo => {
     props.signUp(userInfo);
+  };
+
+  const hasError = meta => {
+    console.log(meta);
+    return "field ui error";
+  };
+
+  const validate = ({ username, email, password }) => {
+    const errors = {};
+    if (!username) {
+      errors.username = "Required";
+    }
+    if (!email) {
+      errors.email = "Required";
+    }
+    if (!password) {
+      errors.password = "Required";
+    } else if (password.length < 6) {
+      errors.password = "Must be longer than 6 characters";
+    }
+    return errors;
   };
 
   return (
@@ -14,27 +36,39 @@ const Signup = props => {
       <div className="ui container">
         <Form
           onSubmit={onSubmit}
-          render={({ handleSubmit, pristine, invalid, submitting }) => (
+          validate={validate}
+          render={({ handleSubmit, pristine, invalid, submitting, errors }) => (
             <form onSubmit={handleSubmit} className="ui form auth-form">
               <h2 className="ui dividing header">Signup</h2>
-              <div className="field">
-                <label>Username</label>
-                <Field
-                  name="username"
-                  component="input"
-                  placeholder="Username"
-                />
-              </div>
-              <div className="field">
-                <label>Email</label>
-                <Field name="email" component="input" placeholder="Email" />
-              </div>
-              <div className="field">
-                <label>Password</label>
+              <div className="fields">
+                <div className="field">
+                  <label>Username</label>
+                  <Field
+                    name="username"
+                    component="input"
+                    placeholder="Username"
+                  />
+                </div>
+                <div className="field">
+                  <label>Email</label>
+                  <Field name="email" component="input" placeholder="Email" />
+                </div>
                 <Field
                   name="password"
-                  component="input"
-                  placeholder="Password"
+                  render={({ meta, input }) => (
+                    <div className="field ui error">
+                      <label>Password</label>
+                      <input
+                        component="input"
+                        placeholder="Password"
+                        type="password"
+                        {...input}
+                      />
+                      {errors.password && (
+                        <p className="ui red message mini">{errors.password}</p>
+                      )}
+                    </div>
+                  )}
                 />
               </div>
               <button
