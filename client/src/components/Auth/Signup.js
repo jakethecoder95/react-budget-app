ad86f84d4674b43bd8af0789fc544997e13b56d6
jakethecoder95 <95jacob07@gmail.com>
2019-04-15 15:26:16 -0700

    Login form client error handling complete

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -2,25 +2,27 @@ import "./Form.css";
 import React, { Fragment } from "react";
 import { Form, Field } from "react-final-form";
 import { connect } from "react-redux";
-import hasError from "../Bottom/Form/util/has-error";
+
+import AuthField from "./AuthField";
+import isEmail from "../Util/regexEmail";
 
 const Signup = props => {
   const onSubmit = userInfo => {
+    console.log(userInfo);
     props.signUp(userInfo);
-  };
-
-  const hasError = meta => {
-    console.log(meta);
-    return "field ui error";
   };
 
   const validate = ({ username, email, password }) => {
     const errors = {};
     if (!username) {
       errors.username = "Required";
+    } else if (username.length < 3) {
+      errors.username = "Must be longer than 2 characters";
     }
     if (!email) {
       errors.email = "Required";
+    } else if (!isEmail(email)) {
+      errors.email = "Please enter a valid email";
     }
     if (!password) {
       errors.password = "Required";
@@ -41,40 +43,15 @@ const Signup = props => {
             <form onSubmit={handleSubmit} className="ui form auth-form">
               <h2 className="ui dividing header">Signup</h2>
               <div className="fields">
-                <div className="field">
-                  <label>Username</label>
-                  <Field
-                    name="username"
-                    component="input"
-                    placeholder="Username"
-                  />
-                </div>
-                <div className="field">
-                  <label>Email</label>
-                  <Field name="email" component="input" placeholder="Email" />
-                </div>
-                <Field
-                  name="password"
-                  render={({ meta, input }) => (
-                    <div className="field ui error">
-                      <label>Password</label>
-                      <input
-                        component="input"
-                        placeholder="Password"
-                        type="password"
-                        {...input}
-                      />
-                      {errors.password && (
-                        <p className="ui red message mini">{errors.password}</p>
-                      )}
-                    </div>
-                  )}
-                />
+                <Field name="username" component={AuthField} />
+                <Field name="email" component={AuthField} />
+                <Field name="password" component={AuthField} />
               </div>
               <button
                 type="submit"
-                disabled={pristine || invalid || submitting}
+                disabled={pristine || submitting}
                 className="ui button primary"
+                style={{ marginTop: "1em" }}
               >
                 Signup
               </button>
