c6267317d253a81e3e60e3a4c4f80ca12be69a54
jakethecoder95 <95jacob07@gmail.com>
2019-05-08 22:14:25 -0700

    started mergeLocalStorageToUserBudget

--- client/src/redux/sagas/get-saved-budget-saga.js
+++ client/src/redux/sagas/get-saved-budget-saga.js
@@ -32,39 +32,41 @@ function* initUserBudget() {
 function* checkLocalStorage() {
   let storedBudget = store.get("budget");
 
-  if (storedBudget) {
-    const currentMonth = new Date().getMonth();
-    const { incomeItems, expenseItems } = storedBudget.items;
+  if (!storedBudget) return;
 
-    // Omit any income item not in the current month
-    const incomes = Object.values(incomeItems).filter(item => {
-      const itemMonth = new Date(item.date).getMonth();
-      if (!item.persist && itemMonth !== currentMonth) {
-        storedBudget.totalIncome -= item.value;
-      }
-      return item.persist || itemMonth === currentMonth;
-    });
+  const currentMonth = new Date().getMonth();
+  const { incomeItems, expenseItems } = storedBudget.items;
 
-    // Omit any expense item not in the current month
-    const expenses = Object.values(expenseItems).filter(item => {
-      const itemMonth = new Date(item.date).getMonth();
-      if (!item.persist && itemMonth !== currentMonth) {
-        storedBudget.totalExpenses -= item.value;
-        storedBudget.catagoryTotals[item.catagory] -= item.value;
-      }
-      return item.persist || itemMonth === currentMonth;
-    });
-
-    if (storedBudget) {
-      yield put({
-        type: SET_INITIAL_BUDGET,
-        payload: {
-          ...storedBudget,
-          items: { incomeItems: { ...incomes }, expenseItems: { ...expenses } }
-        }
-      });
+  // Omit any income item not in the current month
+  const incomes = Object.values(incomeItems).filter(item => {
+    const itemMonth = new Date(item.date).getMonth();
+    if (!item.persist && itemMonth !== currentMonth) {
+      storedBudget.totalIncome -= item.value;
     }
-  }
+    return item.persist || itemMonth === currentMonth;
+  });
+
+  // Omit any expense item not in the current month
+  const expenses = Object.values(expenseItems).filter(item => {
+    const itemMonth = new Date(item.date).getMonth();
+    if (!item.persist && itemMonth !== currentMonth) {
+      storedBudget.totalExpenses -= item.value;
+      storedBudget.catagoryTotals[item.catagory] -= item.value;
+    }
+    return item.persist || itemMonth === currentMonth;
+  });
+
+  yield put({
+    type: SET_INITIAL_BUDGET,
+    payload: {
+      ...storedBudget,
+      items: { incomeItems: { ...incomes }, expenseItems: { ...expenses } }
+    }
+  });
+}
+
+function* mergeLocalStorageToUserBudget(budget) {
+  console.log("here");
 }
 
 export function createGetSavedBudgetSaga() {
